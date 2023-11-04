68baf7223b8b16854de7b46d0947bc48dfb4c0ed
Bryan Armenta <a02300296@usu.edu>
2023-11-03 20:37:29 -0600

    Question 1

---
+++ src/matvec.c
@@ -0,0 +1,23 @@
+/*
+	Multiply a vector by a matrix
+	
+	Input:
+		n: size of square matrix a and vector n
+		m: square (n x n) matrix
+		v: vector
+		
+	Output:
+		p: product m * v
+*/
+void matvec(int n, double m[][n], double v[], double p[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		double sum = 0.0;
+		for (int j = 0; j < n; j++)
+		{
+			sum += m[i][j] * v[j]
+		}
+		p[i] = sum;
+	}
+}
---
+++ src/testbacksub.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "backsub.c"
+#include "matvec.c"
+
+void main() {
+	// Generate 10x10 upper-triangular system of equations
+	// ax = b
+	const int n = 10;
+	double x[n];
+	double a[n][n];
+	for (int i = 0; i < n; i++)
+	{
+		x[i] = 1.0;
+		for (int j = i; j < n; j++)
+		{
+			a[i][j] = (double) rand() / RAND_MAX;
+		}
+	}
+	
+	double b[n];
+	matvec(n, a, x, b);
+	
+	// Solve ax = b
+	printf("Solution should be all 1s:\n[ ");
+	backsub(n, a, b, x); // Re-use x vector, which will be filled with new values
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%.2f ");
+	}
+	printf("]");
+}
