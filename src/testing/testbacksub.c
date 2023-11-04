d4146ac558e5fae03edc44ae4bc3971ab2db6222
Bryan Armenta <a02300296@usu.edu>
2023-11-03 20:54:31 -0600

    Added a new vector to separate real solution from approximation

--- src/testing/testbacksub.c
+++ src/testing/testbacksub.c
@@ -5,15 +5,19 @@
 #include "backsub.c"
 #include "matvec.c"
 
+/*
+	Tests back-substitution on an upper-triangular system of equations
+*/
 void main() {
 	// Generate 10x10 upper-triangular system of equations
-	// ax = b
+	// ay = b
+	// y = [all 1s]
 	const int n = 10;
-	double x[n];
+	double y[n];
 	double a[n][n];
 	for (int i = 0; i < n; i++)
 	{
-		x[i] = 1.0;
+		y[i] = 1.0;
 		for (int j = i; j < n; j++)
 		{
 			a[i][j] = (double) rand() / RAND_MAX;
@@ -21,11 +25,13 @@ void main() {
 	}
 	
 	double b[n];
-	matvec(n, a, x, b);
+	matvec(n, a, y, b);
 	
 	// Solve ax = b
+	// y = x should be the solution
 	printf("Solution should be all 1s:\n[ ");
-	backsub(n, a, b, x); // Re-use x vector, which will be filled with new values
+	double x[n];
+	backsub(n, a, b, x);
 	for (int i = 0; i < n; i++)
 	{
 		printf("%.2f ", x[i]);
