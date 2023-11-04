56e93c22b8ed1165cc2e71f3e1386affd9bb091d
Bryan Armenta <a02300296@usu.edu>
2023-11-03 21:36:32 -0600

    Renamed lufactor.c and created test lufactor

--- src/lufactor.c
+++ src/lufactor.c
@@ -1,11 +1,12 @@
 /*
-LU factorizes the matrix a, where the upper-right and diagonal elements are the upper triangular matrix
-and the lower-left elements are those of the lower triangular matrix
+	LU factorizes the matrix a, where the upper-right and diagonal elements are the upper triangular matrix
+	and the lower-left elements are those of the lower triangular matrix
 
-n: size
-a: matrix
+	Input:
+		n: size of matrix
+		a: n x n matrix
 */
-void lufactorize(int n, double a[][n])
+void lufactor(int n, double a[][n])
 {
     for (int k = 0; k < n - 1; k++)
     {
---
+++ src/testing/testlufactor.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "backsub.c"
+#include "forwardsub.c"
+#include "lufactor.c"
+#include "matvec.c"
+
+/*
+	Tests the routines required for solving a system of equations via Gaussian elimination
+*/
+int main()
+{
+	// Generate strictly diagonal dominant matrix a
+	// System of equations: az = b
+	const int n = 10;
+	double z[n]; // All 1s
+	double a[n][n];
+	for (int i = 0; i < n; i++)
+	{
+		z[i] = 1.0;
+		double sum = 0.0;
+		for (int j = 0; j < n; j++)
+		{
+			a[i][j] = (double) rand() / RAND_MAX;
+			sum += a[i][j];
+		}
+		a[i][i] = sum + (double) rand() / RAND_MAX; // Establish strict diagonal dominance on the row
+	}
+	
+	double b[n];
+	matvec(n, a, z, b);
+	
+	// Lu factor a into a = lu
+	lufactor(n, a);
+	
+	// Solve ly = b
+	double y[n];
+	forwardsub(n, a, b, y);
+	
+	// Solve ux = y
+	double x[n]
+	backsub(n, a, y, x);
+	
+	// Solution should be x = z
+	printf("Solution should be all 1s:\n[ ");
+	for (int i = 0; i < n; i++)
+	{
+		printf("%.2f ", x[i]);
+	}
+	printf("]\n");
+
+	return 0;
+}
